3b1e574e3c907ba8a4a48d390a5b306c798ce551
弓削善史 <yoshifumi@yoshi-bookpro.local>
2018-11-11 01:37:19 +0900

    スライド画面の作りかけ

--- src/app/dream-source/dream-source.page.ts
+++ src/app/dream-source/dream-source.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Slides, NavController, NavParams } from '@ionic/angular';
+import { MockService } from '../mock.service';
+import { Observable } from 'rxjs';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-dream-source',
@@ -7,7 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DreamSourcePage implements OnInit {
 
-  constructor() { }
+  slideOpts = {
+    effect: 'flip'
+  };
+
+  @ViewChild(Slides) slides: Slides;
+  @ViewChild('tabmenu') tabmenu: any;
+  segments: any = [];
+  activeIndex: number = 0;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public mock: MockService) {
+  }
+
+  ionViewDidLoad() {
+    this.segments = this.mock.getSegmentItems();
+  }
+
+  change(idx) {
+    this.activeIndex = idx;
+    this.slides.slideTo(this.activeIndex, 500);
+  }
+  // onSlideChangeStart() {
+  //   let index = this.slides.getActiveIndex();
+  //   // しきい値チェック。タブメニューをはみ出してたらリターン
+  //   if (this.tabmenu.nativeElement.children.length <= index) {
+  //     return;
+  //   }
+  //   this.activeIndex = index;
+
+  //   // タブメニューの移動
+  //   let start = this.tabmenu.nativeElement.scrollLeft;
+  //   let end = this.tabmenu.nativeElement.children[index].offsetLeft;
+  //   let dir = 1;
+  //   // 進行方向を決定(1：進む、-1：戻る)
+  //   if (start > end) {
+  //     dir = dir * -1;
+  //   }
+  //   // 移動量を決める。
+  //   let speed = Math.abs(end - start) / 20;
+  //   let cnt = 0;
+  //   // endの位置に来るまでループ
+  //   let obs = Observable.interval(1).subscribe((x) => {
+  //     cnt = cnt + (speed * dir);
+  //     this.tabmenu.nativeElement.scrollLeft = start + cnt;
+  //     // 進む場合
+  //     if ((start + cnt) >= end && dir == 1) {
+  //       obs.unsubscribe();
+  //     }
+  //     // 戻る場合
+  //     if ((start + cnt) <= end && dir == -1) {
+  //       obs.unsubscribe();
+  //     }
+  //   });
+  // }
 
   ngOnInit() {
   }
